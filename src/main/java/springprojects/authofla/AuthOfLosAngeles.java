97eac28a6ffba7ee17f0eeaf123f8f235383361f
omeryzr_@hotmail.com <omeryzr_@hotmail.com>
2021-11-11 15:32:22 -0800

    created files of project

---
+++ src/main/java/springprojects/authofla/AuthOfLosAngeles.java
@@ -0,0 +1,13 @@
+package springprojects.authofla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthOfLosAngeles {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthOfLosAngeles.class, args);
+	}
+
+}
---
+++ src/test/java/springprojects/authofla/AuthOfLosAngelesTests.java
@@ -0,0 +1,13 @@
+package springprojects.authofla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthOfLosAngelesTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
