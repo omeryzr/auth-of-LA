e23ed6131197ece1a3d27cb1baec8c7a17b005b3
omeryzr_@hotmail.com <omeryzr_@hotmail.com>
2021-11-11 22:28:11 -0800

    set disable to csrf security and created two example controller

---
+++ src/main/java/springprojects/authofla/controllers/UserController.java
@@ -0,0 +1,22 @@
+package springprojects.authofla.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @GetMapping("/examplePath")
+    public String showToFatih(){
+        return "This method is working Fatih.";
+    }
+
+    @GetMapping("/examplePath1")
+    public String secondMethodinFirstExample(){
+        return "same page second module";
+    }
+}
+
+
+
