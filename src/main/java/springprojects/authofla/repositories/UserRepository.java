0ab0dfd1f8a094c266c8730a4372bcdbc38e8059
omeryzr_@hotmail.com <omeryzr_@hotmail.com>
2021-11-12 15:28:00 -0800

    Created Service and Repository module for UserInformation 	created method to find user by email - (findByEmail) 	created properties and defined allArgsConstructor spring anotation 	overrided loadUserByUsername module from UserDetailservice and configured

---
+++ src/main/java/springprojects/authofla/repositories/UserRepository.java
@@ -0,0 +1,14 @@
+package springprojects.authofla.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import springprojects.authofla.entities.UserInformation;
+
+import java.util.Optional;
+
+@Repository
+@Transactional(readOnly = true)
+public interface UserRepository extends JpaRepository<UserInformation, Long> {
+    Optional<UserInformation> findByEmail(String email);
+}
