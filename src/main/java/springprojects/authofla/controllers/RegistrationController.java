128d4aab2212fc6451ce01a4dc2d4d2aa903b191
omeryzr_@hotmail.com <omeryzr_@hotmail.com>
2021-11-12 22:51:57 -0800

    Created Pasword Encoder - Created register path - created web security config class Created Web Security Config and extented from WebSecurityConfigurerAdapter csrf configuration disabled and set all files  permission

--- src/main/java/springprojects/authofla/controllers/RegistrationController.java
+++ src/main/java/springprojects/authofla/controllers/RegistrationController.java
@@ -2,6 +2,7 @@ package springprojects.authofla.controllers;
 
 
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ public class RegistrationController {
 
     private RegistrationService registrationService;
 
+    @PostMapping
     public String register(@RequestBody RegistrationRequest request){
         return registrationService.register(request);
 
