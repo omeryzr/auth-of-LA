595588cb941ee891355c49d148aea1076447d89b
omeryzr_@hotmail.com <omeryzr_@hotmail.com>
2021-11-15 18:19:21 -0800

    created updateConfirmedAt and enableUser methods created updateConfirmedAt method to change token status created enableUser method to update enable status of User - false to true

--- src/main/java/springprojects/authofla/repositories/UserRepository.java
+++ src/main/java/springprojects/authofla/repositories/UserRepository.java
@@ -1,6 +1,8 @@
 package springprojects.authofla.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import springprojects.authofla.entities.User;
@@ -11,4 +13,11 @@ import java.util.Optional;
 @Transactional(readOnly = true)
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE User a " +
+           "SET a.enabled = TRUE WHERE a.email = ?1")
+    int enableUser(String email);
+
 }
