aae45a7c2caf64aa4a18f5fbfba1dd73a43ba3cd
omeryzr_@hotmail.com <omeryzr_@hotmail.com>
2021-11-16 12:10:11 -0800

    created getToken and setConfirmedAt methods the getToken method works to get user token the setConfirmedAt method works to set user token with be confirmed token

--- src/main/java/springprojects/authofla/services/ConfimationTokenService.java
+++ src/main/java/springprojects/authofla/services/ConfimationTokenService.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Service;
 import springprojects.authofla.entities.ConfirmationToken;
 import springprojects.authofla.repositories.ConfirmationTokenRepository;
 
+import java.time.LocalDateTime;
+import java.util.Optional;
+
 @Service
 @AllArgsConstructor
 public class ConfimationTokenService {
@@ -12,6 +15,15 @@ public class ConfimationTokenService {
 
     public void saveConfirmationToken(ConfirmationToken confirmationToken){
         confirmationTokenRepository.save(confirmationToken);
+    }
+
+    public Optional<ConfirmationToken> getToken(String token) {
+        return confirmationTokenRepository.findByToken(token);
+    }
+
+    public int setConfirmedAt(String token) {
+        return confirmationTokenRepository.updateConfirmedAt(
+                token, LocalDateTime.now());
     }
 }
 
