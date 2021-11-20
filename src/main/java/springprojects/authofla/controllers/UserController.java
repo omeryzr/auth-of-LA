e15c7ad331c0d79b550c0c320adec9afe5108dcb
omeryzr_@hotmail.com <omeryzr_@hotmail.com>
2021-11-19 18:32:11 -0800

    created UserController class and MyProfile get controller to show current user own user information

--- src/main/java/springprojects/authofla/controllers/UserController.java
+++ src/main/java/springprojects/authofla/controllers/UserController.java
@@ -1,4 +1,25 @@
 package springprojects.authofla.controllers;
 
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import springprojects.authofla.entities.User;
+import springprojects.authofla.repositories.UserRepository;
+
+import java.security.Principal;
+import java.util.Optional;
+
+@RestController
+@RequestMapping(path = "api/v1/users/")
+@AllArgsConstructor
 public class UserController {
+
+    private final UserRepository userRepository;
+
+
+    @GetMapping("/profile")
+    public Optional<User> myProfile(Principal principal){
+        return userRepository.findByEmail(principal.getName());
+    }
 }
