24a461d6e783c078049a20f9de573e1aa9c88dc4
Omer Yazir <omer.yazir@Catalysts-MacBook-Pro.local>
2021-11-24 09:47:48 -0800

    created a method to update user information but on controller class

--- src/main/java/springprojects/authofla/controllers/UserController.java
+++ src/main/java/springprojects/authofla/controllers/UserController.java
@@ -1,12 +1,11 @@
 package springprojects.authofla.controllers;
 
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import springprojects.authofla.entities.User;
 import springprojects.authofla.repositories.UserRepository;
+import springprojects.authofla.services.UserService;
 
 import java.security.Principal;
 import java.util.Optional;
@@ -17,6 +16,7 @@ import java.util.Optional;
 public class UserController {
 
     private final UserRepository userRepository;
+    private final UserService userService;
 
 
     @GetMapping("/profile")
@@ -27,5 +27,14 @@ public class UserController {
     @GetMapping("/profile/{id}")
     public Optional<User> showUserById(@PathVariable Long id){
         return userRepository.findById(id);
+    }
+
+    @PutMapping("/profile/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable(value = "id") Long id, @RequestBody User user){
+        User userInformation = userRepository.getById(id);
+        userInformation.setFirstName(user.getFirstName());
+        userInformation.setLastName(user.getLastName());
+        final User updatedUser = userRepository.save(userInformation);
+        return ResponseEntity.ok(updatedUser);
     }
 }
